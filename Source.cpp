bcc597535102ac61ee6b7101c422b6116f1e3811
Oleksandr Zabila <71663207+OLZABILA@users.noreply.github.com>
2020-10-14 19:45:59 +0300

    home work 2/4

---
+++ Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+void main()
+{
+	double a, b, c;
+	cout << " Enter 3 = ";
+	cin >> a >> b >> c;
+	if (a == b)cout << "A = " << a << "\n B = "  << b;
+	if (a == c)cout << "\nA = " << a << "\nC = "  << c;
+	if (b == c)cout << "\nB = " << b <<  "\nC = " << c;
+
+
+
+
+
+}
