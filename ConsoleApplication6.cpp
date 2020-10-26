53fb2ab88d093215ee6927611c5dee4152aec0d7
Oleksandr Zabila <71663207+OLZABILA@users.noreply.github.com>
2020-10-26 23:37:06 +0300

    Add files via upload
    2/19 дано три целых числа. Вознести в квадрат отрицательные числа и в третию степень положытельные. (Число ноль не изменяется)

---
+++ ConsoleApplication6.cpp
@@ -0,0 +1,59 @@
+﻿#include <cmath>
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a,b,c;
+	cin >> a;
+	if (a > 0)
+	{
+		a = pow(a, 3);
+		cout << a;
+	}
+	else
+	{
+		if (a == 0)
+			cout << a;
+		else
+		{
+			if (a < 0)
+				a = pow(a, 2);
+			cout << a;
+		}
+	}
+	if (b > 0)
+	{
+		b = pow(b, 3);
+		cout << b;
+	}
+	else
+	{
+		if (b == 0)
+			cout << b;
+		else
+		{
+			if (b < 0)
+				b = pow(b, 2);
+			cout << b;
+		}
+	}
+	if (c > 0)
+	{
+		c = pow(c, 3);
+		cout << c;
+	}
+	else
+	{
+		if (c == 0)
+			cout << c;
+		else
+		{
+			if (c < 0)
+				c = pow(c, 2);
+			cout << c;
+		}
+	}
+
+	
+
+}
